a75bedbf3a00de8bea3e50c913ba32284c2aa0be
Chuong Nguyen <xuanchuongdp@gmail.com>
2021-05-10 10:57:51 +0700

    [master] init domain module

---
+++ domain/src/main/java/xuanchuongdp/domain/entity/User.java
@@ -0,0 +1,14 @@
+package xuanchuongdp.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class User {
+    String firstName;
+    String lastName;
+}
---
+++ domain/src/main/java/xuanchuongdp/domain/entity/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package xuanchuongdp.domain.entity.repository;
+
+import xuanchuongdp.domain.entity.User;
+
+public interface UserRepository {
+    User getUserById(Long id);
+}
