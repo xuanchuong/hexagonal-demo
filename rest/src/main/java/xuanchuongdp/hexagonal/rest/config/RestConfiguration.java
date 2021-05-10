c42a4c3f29f76d4eda660ff9b72064a204eb309b
Chuong Nguyen <xuanchuongdp@gmail.com>
2021-05-10 17:07:30 +0700

    [master] export swagger UI

--- rest/src/main/java/xuanchuongdp/hexagonal/rest/config/RestConfiguration.java
+++ rest/src/main/java/xuanchuongdp/hexagonal/rest/config/RestConfiguration.java
@@ -2,6 +2,10 @@ package xuanchuongdp.hexagonal.rest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import xuanchuongdp.hexagonal.domain.repository.UserRepository;
 import xuanchuongdp.hexagonal.rest.endpoint.UserRest;
 
@@ -11,5 +15,14 @@ public class RestConfiguration {
     @Bean
     public UserRest userRest(UserRepository userRepository) {
         return new UserRest(userRepository);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
     }
 }
