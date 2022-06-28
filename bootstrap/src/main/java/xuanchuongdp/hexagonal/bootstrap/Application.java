b85a92872b8be16982117f740510a92e44bc84e4
xuanchuongdp <xuanchuongdp@gmail.com>
2022-06-28 14:53:27 +0700

    Update Application.java

--- bootstrap/src/main/java/xuanchuongdp/hexagonal/bootstrap/Application.java
+++ bootstrap/src/main/java/xuanchuongdp/hexagonal/bootstrap/Application.java
@@ -13,7 +13,7 @@ import xuanchuongdp.hexagonal.rest.config.RestConfiguration;
     RestConfiguration.class
 })
 public class Application {
-    // test 14:51
+    // test 14:53
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
